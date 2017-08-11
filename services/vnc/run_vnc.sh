884a481616a3d069a2d2b1747f26f1b3335eabec
Stefano Alberto Russo <stefano.russo@gmail.com>
2017-08-11 19:18:46 +0100

    Decoupled vnc from twm service, added support for setting user at runtime for vnc.

---
+++ services/vnc/run_vnc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# If VNC_USER is not set, derive it from instance.
+if [[ "x$VNC_USER" == "x" ]] ; then
+    VNC_USER=$INSTANCE    
+fi
+
+# If user does not exists, fallback to metabox user
+if [[ "x$(id -u $VNC_USER)" == "x" ]] ; then
+	echo "WARNING: user \"$VNC_USER\" does not exist, falling back on \"metabox\" user"
+    VNC_USER=metabox
+fi
+
+echo "VNC_USER: $VNC_USER"
+
+# Exec TigerVNC server as this instance user
+sudo -i -u $VNC_USER /opt/tigervnc/usr/bin/vncserver :0 -SecurityTypes None -xstartup /opt/tigervnc/xstartup
+
+# Check it is running and if no, exit
+while true
+do
+
+    PSOUT=$(ps -ef | grep /opt/tigervnc/usr/bin/Xvnc | grep SecurityTypes) 
+
+    if [[ "x$PSOUT" == "x" ]] ; then
+        exit 1
+    fi
+
+	# Sleep other 10 secs before re-checking
+	sleep 10
+
+done
