0304974047dae6b2516f916035f02749558db204
Stefano Alberto Russo <stefano.russo@gmail.com>
2017-08-21 00:06:46 +0100

    Added hamachi and vpn (through hamachi) services

---
+++ services/hamachi/run_Hamachi.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+start_daemon(){
+  /opt/logmein-hamachi/bin/hamachid -c /data/hamachi_config
+  while [ 1 ]; do
+    out=$(hamachi)
+    [[ $out =~ *version* ]] && sleep 1 || break
+  done
+}
+
+check_login(){
+  IFS=$'\n';
+  regex_status="status.*?: (.*?)"
+  regex_account="lmi account:(.*?)"
+  for line in $(hamachi); do
+    if [[ $line =~ $regex_status ]]; then
+      if [[ ${BASH_REMATCH[1]} == offline ]]; then
+        while [ 1 ]; do
+          out=$(hamachi login)
+          [[ $out =~ ok ]] && break || sleep 1
+        done
+      fi
+    fi
+  done
+  while [ 1 ]; do
+    out=$(hamachi)
+    [[ $out =~ "loggin in" ]] && sleep 1
+    [[ $out =~ "logged in" ]] && break
+  done
+}
+
+check_account(){
+  IFS=$'\n';
+  regex_status="status.*?: (.*?)"
+  regex_account="lmi account: (.*)"
+  for line in $(hamachi); do
+    if [[ $line =~ $regex_account ]]; then
+      if [[ ${BASH_REMATCH[1]} != $ACCOUNT ]]; then
+        hamachi attach $ACCOUNT
+      fi
+    fi
+  done
+}
+
+start_daemon
+#check_login
+#check_account
+
+# Give some time to the daemon
+sleep 5
+
+while pgrep hamachid >/dev/null; do
+  sleep 10
+done
