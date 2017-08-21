c056dbe5cea2428d9a81d02a7dc585b3fdcc7901
Stefano Alberto Russo <stefano.russo@gmail.com>
2017-08-21 02:18:49 +0100

    Added prestartup script for creating tun device if not existent

---
+++ services/hamachi/prestartup_hamachi.sh
@@ -0,0 +1,8 @@
+# Nothing here..
+
+if [ ! -e /dev/net/tun ] ; then
+        echo Creating TunTap ..
+        mkdir -p /dev/net
+        mknod /dev/net/tun c 10 200
+        chmod 0666 /dev/net/tun
+fi
